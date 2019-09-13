d343d07d7260867e14713c5b16adb9ba570c2952
Jason Fry <jmphry@gmail.com>
2019-09-13 13:48:32 -0400

    Add frontend boilerplate
    This is just basic code to ensure dev env is setup correctly.
    
    Resolves #2

---
+++ src/frontend/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (..)
+
+import Browser
+import Html
+
+
+main =
+    Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    { patientId : Int
+    , patientName : String
+    , patientNote : String
+    , patientTime : Int
+    }
+
+
+type Msg
+    = NoOp
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { patientId = 0
+      , patientName = ""
+      , patientNote = ""
+      , patientTime = 0
+      }
+    , Cmd.none
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div [] []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
