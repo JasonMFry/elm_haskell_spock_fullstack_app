8a2bbb61f074df4eb3925312ca60d0c4f649d99e
Jason Fry <jmphry@gmail.com>
2019-09-14 11:11:17 -0400

    Move api functions into its own module
    This resolves #10

--- src/frontend/Main.elm
+++ src/frontend/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Api
 import Browser
 import Html
 import Http
@@ -22,34 +23,8 @@ main =
 init : () -> ( Model.Model, Cmd Msg.Msg )
 init _ =
     ( Model.Loading
-    , getAllPatients
+    , Api.getAllPatients
     )
-
-
-
--- HTTP
-
-
-getAllPatients : Cmd Msg.Msg
-getAllPatients =
-    Http.get
-        { url = "http://localhost:8080/patients" -- shouldn't hardcode this in prod for sure.
-        , expect = Http.expectJson Msg.FetchPatients (D.list patientDecoder)
-        }
-
-
-patientDecoder : D.Decoder Model.Patient
-patientDecoder =
-    D.map4 Model.Patient
-        (D.field "id" D.int)
-        patientNameDecoder
-        (D.field "note" D.string)
-        (D.field "seconds" D.int)
-
-
-patientNameDecoder : D.Decoder Model.PatientName
-patientNameDecoder =
-    D.map Model.PatientName (D.field "name" D.string)
 
 
 
